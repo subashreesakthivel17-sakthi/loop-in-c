0d018f426cc1376c1523ec14c8e82fef91dd5f21
subashree <subashreesakthivel17@gmail.com>
2026-02-16 10:05:25 +0530

    Add files via upload

---
+++ two multiply in loop.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int n,m;
+    int result;
+    scanf("%d %d",&n,&m);
+    for(int i=1;i<m;i++){
+        result=n*m;
+    }
+    printf("%d",result);
+    
+    
+    return 0;
+}
