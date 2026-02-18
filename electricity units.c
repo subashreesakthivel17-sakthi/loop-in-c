20e0549a63b11cbac32f4b2d169bc64870f378bc
subashree <subashreesakthivel17@gmail.com>
2026-02-18 10:29:44 +0530

    Add files via upload

---
+++ electricity units.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+   int  n,unit;
+   int totalunit=0;
+   scanf("%d",&n);
+   for(int i=0;i<n;i++)
+   {
+      scanf("%d",&unit);
+      totalunit+=unit;
+   }
+       if(n+=unit){
+          totalunit;
+       }
+       printf("Total unit:%d",totalunit);
+      
+    
+
+   
+    return 0;
+}
